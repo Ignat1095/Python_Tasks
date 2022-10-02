e4c049eecaf658f9e9c5fcf97ac7246243ee4a13
Ignat <Ignat1095@mail.ru>
2022-10-02 17:14:45 +0300

    add Task_2

---
+++ Task_2/1_Сумма цифр.py
@@ -0,0 +1,20 @@
+# Напишите программу, которая принимает на вход
+# вещественное число и показывает сумму его цифр.
+
+# Пример:
+
+# - 6782 -> 23
+# - 0,56 -> 11
+
+number = (input("input mumber: "))
+sum = 0
+for i in number:
+    sum += int(i)
+print("sum = ", sum)
+
+
+# def sum(n): # Рекурсией
+#     if n < 10:
+#         return n
+#     return n % 10 + sum(n // 10)
+# print(sum(int(input("number = "))))
